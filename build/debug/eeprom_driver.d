08c7da68990e16ec0eb1d1afe9dde7f7b2e6276e
Your Name <you@example.com>
2026-07-22 18:29:53 -0500

    Initial commit: display improvements and render loop

---
+++ build/debug/eeprom_driver.d
@@ -0,0 +1,8 @@
+build/debug/eeprom_driver.o: eeprom_driver.c eeprom_driver.h types.h \
+ board_config.h i2c.h config.h pinout.h
+eeprom_driver.h:
+types.h:
+board_config.h:
+i2c.h:
+config.h:
+pinout.h:
